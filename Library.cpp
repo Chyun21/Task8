8335510985baacd2e134fe1610cb8e09faf0362f
Chyun21 <75759908+Chyun21@users.noreply.github.com>
2023-12-01 11:08:08 -0500

    edited files

--- Library.cpp
+++ Library.cpp
@@ -1,3 +1,43 @@
 //include headers and libraries
-
+#include <iostream>
+#include <array>
+#include <string>
+#include "Library.h"
+using namespace std;
 //implement class functions
+Library::Library(){
+    string books[10];
+}
+bool Library::addBook(string bookName) {
+    for(int i=0; i<10; i++){
+        if(books[i] == ""){
+            books[i] = bookName;
+            return true;
+        }
+
+    }
+    cout << "Cannot add the book.";
+    return false;
+}
+
+bool Library::removeBook(string bookName) {
+    for (int i = 0; i <= 10; i++) {
+        if (books[i] == bookName) {
+            books[i] = "";
+            return true;
+        }
+    }
+    cout << "Cannot remove the book.";
+    return false;
+}
+
+void Library::print() {
+    cout << "Library contents: \n";
+    for (int i = 0; i < 10; i++) {
+        if (books[i] != "") {
+            cout << books[i] << endl;
+        }
+    }
+}
+
+
--- P1.cpp
+++ P1.cpp
@@ -1,8 +1,35 @@
 //include headers and libraries
-
+#include <iostream>
+#include <array>
+#include <string>
+#include "Library.h"
+using namespace std;
 int main(){
     Library library;
     
     //ask for commands and execute
+    string command;
+    string bookName;
+    while(true){
+        cout << "Enter command: ";
+        cin >> command;
+        if(command == "add"){
+            cin >> bookName;
+            library.addBook(bookName);
+        }
+        else if(command == "remove"){
+            cin >> bookName;
+            library.removeBook(bookName);
+        }
+        else if(command == "print"){
+            library.print();
+        }
+        else if(command == "quit"){
+            break;
+        }
+        else{
+            cout << "Invalid command." << endl;
+        }
+    }
 
 }
